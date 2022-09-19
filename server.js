2abb14cac8e508d3ca913835117c3ca5b2bf21d2
Subhasish Biswasray <subhasish9583672357@gmail.com>
2022-09-19 17:37:57 +0530

    refactor(sockets): moved entire socket code

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 require('dotenv').config();
+require('./config/database').connect();
 const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const io = require('socket.io')(server);
-require('./config/database').connect();
-const Chat = require('./models/chat');
-const port = process.env.PORT || 5656;
-const msgLimit = 5;
-//var clients=0;
-var clients = [];
-var userMap = new Map();
-var tempMap = new Map();
+const {chatSocket} = require('./sockets/chatSocket');
 
 app.use(express.json());
 
@@ -22,37 +15,8 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-const nsp = io.of('/chat');
-nsp.on('connection', async (socket) => {
-    let nchats = 0;
-    nchats = await Chat.count();
-    clients.push(socket.id);
-    nsp.emit('no_client', { type: 'serverMessage', message: clients.length + " clients connected!" });
-
-    socket.on('setUser', (data) => {
-        if (userMap.has(data))
-            return socket.emit('userExist', data + " is already exist <br/> Try new name");
-        userMap.set(data, socket.id);
-        tempMap.set(socket.id, data);
-        socket.emit('userSet', data);
-    });
-    socket.on('msg', async (data) => {
-        nsp.emit('newMsg', data);
-        let d = await Chat.create(data);
-    });
-    socket.on('loadPrev', async (d) => {
-        let prevm = await Chat.find().skip((nchats - msgLimit) < 0 ? 0 : (nchats - msgLimit)).limit(msgLimit);
-        socket.emit('prev_message', prevm);
-        nchats = nchats - msgLimit;
-    });
-    socket.on('disconnect', function () {
-        let i = clients.indexOf(socket.id);
-        clients.splice(i, 1);
-        userMap.delete(tempMap.get(socket.id));
-        tempMap.delete(socket.id);
-        nsp.emit('no_client', { type: 'serverMessage', message: clients.length + " clients connected!" });
-    });
-});
+chatSocket(server);
+const port = process.env.PORT || 5656;
 
 server.listen(port, () => console.log("server start..."));
 //Welcome to LolMegle ,this is an open public opinion and discussion application . Anyone can share their opinion . (Note-Please don't use slangs ,Thank you)
---
+++ sockets/chatSocket.js
@@ -0,0 +1,44 @@
+const socketServer = require('socket.io');
+const Chat = require('../models/chat');
+const port = process.env.PORT || 5656;
+const msgLimit = 5;
+//var clients=0;
+var clients = [];
+var userMap = new Map();
+var tempMap = new Map();
+function chatSocket(server){
+    const io = socketServer(server);
+    const nsp = io.of('/chat');
+    nsp.on('connection', async (socket) => {
+        let nchats = 0;
+        nchats = await Chat.count();
+        clients.push(socket.id);
+        nsp.emit('no_client', { type: 'serverMessage', message: clients.length + " clients connected!" });
+
+        socket.on('setUser', (data) => {
+            if (userMap.has(data))
+                return socket.emit('userExist', data + " is already exist <br/> Try new name");
+            userMap.set(data, socket.id);
+            tempMap.set(socket.id, data);
+            socket.emit('userSet', data);
+        });
+        socket.on('msg', async (data) => {
+            nsp.emit('newMsg', data);
+            let d = await Chat.create(data);
+        });
+        socket.on('loadPrev', async (d) => {
+            let prevm = await Chat.find().skip((nchats - msgLimit) < 0 ? 0 : (nchats - msgLimit)).limit(msgLimit);
+            socket.emit('prev_message', prevm);
+            nchats = nchats - msgLimit;
+        });
+        socket.on('disconnect', function () {
+            let i = clients.indexOf(socket.id);
+            clients.splice(i, 1);
+            userMap.delete(tempMap.get(socket.id));
+            tempMap.delete(socket.id);
+            nsp.emit('no_client', { type: 'serverMessage', message: clients.length + " clients connected!" });
+        });
+    });
+}
+
+module.exports = {chatSocket};
