b2b30dffae63301688d54accbc0e8b838edde7c6
Subhasish Biswasray <subhasish.biswasray@theinternetfolks.com>
2022-09-26 21:57:10 +0530

    feat:added room html

--- server.js
+++ server.js
@@ -2,14 +2,20 @@ require('dotenv').config();
 require('./config/database').connect();
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
 const http = require('http');
 const server = http.createServer(app);
 const { socketio } = require('./sockets/socketio');
 
+app.use(cookieParser());
 app.use(express.json());
 
 app.get('/favicon.ico', (req, res) => {
     res.sendFile(__dirname + '/public/images/favicon.ico');
+});
+app.get('/room', (req, res) => {
+    res.cookie('test','Hello World');
+    res.sendFile(__dirname + '/public/room.html');
 });
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
