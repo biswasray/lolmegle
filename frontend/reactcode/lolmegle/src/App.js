3a6c1b371e57482fdcbdc42b07e05ffd7ebf255a
Subhasish Biswasray <subhasish9583672357@gmail.com>
2022-10-09 00:18:17 +0530

    Added socket-client io in AuthContext.
    Co-authored-by: SK skmafarali@gmail.com

--- frontend/reactcode/lolmegle/src/App.js
+++ frontend/reactcode/lolmegle/src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import useAuth from "./context/useAuth";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  const { socket } = useAuth();
+  const [no_client,setNo_client] = useState(0);
+
+  useEffect(() => {
+    socket.on("no_client", (data) => {
+      setNo_client(parseInt(data.message));
+    });
+  }, [socket]);
+  return <div className="App">{no_client}</div>;
 }
 
 export default App;
--- frontend/reactcode/lolmegle/src/context/AuthProvider.js
+++ frontend/reactcode/lolmegle/src/context/AuthProvider.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 import AuthContext from './AuthContext'
+import io from 'socket.io-client'
 function AuthProvider(props) {
     const [auth,setAuth]=useState(localStorage.getItem("meUser")?JSON.parse(localStorage.getItem("meUser")):null);
+    const [socket]=useState(io('http://localhost:5656/chat'));
   return (
-    <AuthContext.Provider value={{auth,setAuth}}>
+    <AuthContext.Provider value={{auth,setAuth,socket}}>
         {props.children}
     </AuthContext.Provider>
   )
